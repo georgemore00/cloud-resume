1e958a771cca8db922ae893f97ad61571a4587d4
George moré <georgemore00@hotmail.com>
2023-07-10 00:52:26 +0200

    added multi-line slashes to aws deploy command

--- backend/src/get_page_visits.py
+++ backend/src/get_page_visits.py
@@ -5,7 +5,7 @@ import boto3
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('pageVisits')
 
-# For now it does not count unique page-visits
+# Currently it does not count unique page-visits
 def lambda_handler(event, context):
     response = table.update_item(
         Key={
