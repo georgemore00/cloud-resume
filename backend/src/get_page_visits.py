863b757893be03f056425f31ec4e1b73937b261f
George moré <georgemore00@hotmail.com>
2023-07-10 10:16:28 +0200

    added CORS headers in lambda response

--- backend/src/get_page_visits.py
+++ backend/src/get_page_visits.py
@@ -22,5 +22,10 @@ def lambda_handler(event, context):
 
     return {
         'statusCode': 200,
+        'headers': {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "*", 
+            "Access-Control-Allow-Methods": "GET" 
+        },
         'body': json.dumps(f'visitors: {visitors_count}')
     }
